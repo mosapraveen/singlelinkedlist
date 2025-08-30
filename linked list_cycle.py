806f98908b844642d655718e6428a039d43bc5b7
MOSA PRAVEEN <160846996+mosapraveen@users.noreply.github.com>
2025-08-31 01:51:44 +0530

    Create linked list_cycle.py

---
+++ linked list_cycle.py
@@ -0,0 +1,49 @@
+#linked list cycle
+
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class singlelist:
+    def __init__(self):
+        self.head=None
+
+    def traversal(self):
+        if self.head is None:
+            print("list is empty")
+        a=self.head
+        while a is not None:
+            print(a.data,end=" ")
+            a=a.next
+
+    def cycle(self):
+        slow=self.head
+        fast=self.head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+            if(fast==slow):
+                return True
+        return False
+
+node=singlelist()
+node.traversal()
+n1=Node(10)
+n2=Node(12)
+n3=Node(13)
+n4=Node(14)
+n5=Node(10)
+n6=Node(14)
+n7=Node(13)
+node.head=n1
+n1.next=n2
+n2.next=n3
+n3.next=n4
+n4.next=n5
+n5.next=n6
+n6.next=n7
+n7.next=n3
+node.cycle()
+ 
